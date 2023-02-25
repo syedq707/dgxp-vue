fb58886331b1914cdd35b8ae3e6443e201b1d814
Web <syed.yousuf@navana.com>
2023-02-25 17:08:45 +0600

    Headerm links updated

--- dgxp-landing/src/components/LandingHeader.vue
+++ dgxp-landing/src/components/LandingHeader.vue
@@ -1,228 +1,230 @@
 <template>
-    <header id="header">
-            <div class="boxed">
-                <a href="https://digitalgxp.com/" class="logo">
-                    <img id="logo" src="../assets/logo/DGxP-Logo.png" alt="Digital GxP logo">
-                </a>
+  <header id="header">
+    <div class="boxed">
+      <a href="https://digitalgxp.com/" class="logo">
+        <img
+          id="logo"
+          src="../assets/logo/DGxP-Logo.png"
+          alt="Digital GxP logo"
+        />
+      </a>
 
-                <nav id="primary-menu" v-bind:class="{active: active}">
-                    <ul>
-                        <li><a class="navlinks" href="https://digitalgxp.com/literature/">Literature</a></li>
-                        <li><a class="navlinks" href="https://digitalgxp.com/blogs/">Blogs</a></li>
-                        <li><a class="navlinks" href="https://digitalgxp.com/login">Login</a></li>
-                    </ul>
-                </nav>
+      <nav id="primary-menu" v-bind:class="{ active: active }">
+        <ul>
+          <li>
+            <a class="navlinks" href="https://digitalgxp.com/blog/literature/"
+              >Literature</a
+            >
+          </li>
+          <li>
+            <a class="navlinks" href="https://digitalgxp.com/blog/">Blogs</a>
+          </li>
+          <li>
+            <a class="navlinks" href="https://digitalgxp.com/app/login"
+              >Login</a
+            >
+          </li>
+        </ul>
+      </nav>
 
-                <div id="toggle-btn" v-on:click="toggleMobileMenu" v-bind:class="{active: active}">
-                    <div class="line1"></div>
-                    <div class="line2"></div>
-                    <div class="line3"></div>
-                </div>
-            </div>
-    </header>
-
+      <div
+        id="toggle-btn"
+        v-on:click="toggleMobileMenu"
+        v-bind:class="{ active: active }"
+      >
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
- export default {
-    name: 'LandingHeader',
-        data() {
-        return {
-            active: false
-        }
-    },
-    created () {
-        if (typeof window !== "undefined") {
-            window.addEventListener('scroll', this.handleScroll);
-        }
-    },
-    unmounted () {
-        if (typeof window !== "undefined") {
-            window.removeEventListener('scroll', this.handleScroll);
-        }
-    },
-    methods: {
-        handleScroll () {
-            if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-                document.getElementById("header").classList.add('shrink');
-            } else {
-                document.getElementById("header").classList.remove('shrink');
-            }
-        },
-        toggleMobileMenu() {
-            const navlinks = document.querySelectorAll(".navlinks");
-
-            this.active = !this.active;
-
-            // Animate Nav Text
-            navlinks.forEach((link, index) => {
-                if (link.style.animation) {
-                    link.style.animation = ``;
-                }
-                else {
-                    link.style.animation = `navLinksFade 0.5s ease backwards ${index / 6 + 0.4}s`;
-                }
-            });
-
-
-        }
+export default {
+  name: "LandingHeader",
+  data() {
+    return {
+      active: false,
+    };
+  },
+  created() {
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", this.handleScroll);
     }
- }
+  },
+  unmounted() {
+    if (typeof window !== "undefined") {
+      window.removeEventListener("scroll", this.handleScroll);
+    }
+  },
+  methods: {
+    handleScroll() {
+      if (
+        document.body.scrollTop > 50 ||
+        document.documentElement.scrollTop > 50
+      ) {
+        document.getElementById("header").classList.add("shrink");
+      } else {
+        document.getElementById("header").classList.remove("shrink");
+      }
+    },
+    toggleMobileMenu() {
+      const navlinks = document.querySelectorAll(".navlinks");
+
+      this.active = !this.active;
+
+      // Animate Nav Text
+      navlinks.forEach((link, index) => {
+        if (link.style.animation) {
+          link.style.animation = ``;
+        } else {
+          link.style.animation = `navLinksFade 0.5s ease backwards ${
+            index / 6 + 0.4
+          }s`;
+        }
+      });
+    },
+  },
+};
 </script>
 
 <style>
+/*---------------------------------  Custom Element Styling  ----------------------------*/
 
+.logo img {
+  width: 80px;
+  height: auto;
+  transition: all 0.2s ease-in;
+}
 
+.shrink .logo img {
+  width: 60px;
+  height: auto;
+}
 
-    /*---------------------------------  Custom Element Styling  ----------------------------*/
+/* Mobile Menu & Burger Icon Styling Here */
 
-    .logo img {
-        width: 80px;
-        height: auto;
-        transition: all 0.2s ease-in;
-    }
+#toggle-btn {
+  display: block;
+}
 
-    .shrink .logo img {
-        width: 60px;
-        height: auto;
-    }
+#toggle-btn div {
+  width: 25px;
+  height: 2px;
+  background-color: #000;
+  margin: 5px;
 
-    /* Mobile Menu & Burger Icon Styling Here */
+  transition: all 0.3s ease-in-out 0.3s;
+  z-index: 3000;
+}
 
-    #toggle-btn {
-        display: block;
-    }
+#toggle-btn.active div {
+  background-color: #fff;
+}
 
-    #toggle-btn div {
-        width: 25px;
-        height: 2px;
-        background-color: #000;
-        margin: 5px;
+#toggle-btn.active .line1 {
+  transform: rotate(-45deg) translate(-5px, 6px);
+}
 
-        transition: all 0.3s ease-in-out 0.3s;
-        z-index: 3000;
-    }
+#toggle-btn.active .line2 {
+  opacity: 0;
+}
 
-    #toggle-btn.active div {
-        background-color: #fff;
-    }
+#toggle-btn.active .line3 {
+  transform: rotate(45deg) translate(-5px, -6px);
+}
 
-    #toggle-btn.active .line1 {
-        transform: rotate(-45deg) translate(-5px, 6px);
-    }
+header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: stretch;
 
-    #toggle-btn.active .line2 {
-        opacity: 0;
-    }
+  transition: all 0.2s ease-in;
+  width: 100%;
+  padding: 1rem 0;
+  z-index: 2000;
+}
 
-    #toggle-btn.active .line3 {
-        transform: rotate(45deg) translate(-5px, -6px);
-    }
+header.shrink {
+  padding: 0.5rem 0;
+  background-color: #000;
+}
 
+header.shrink #toggle-btn div {
+  background-color: #fff;
+}
 
-    header {
-        position: fixed;
-        top: 0;
-        left: 0;
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
-        align-items: stretch;
+header .boxed {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
 
-        transition: all 0.2s ease-in;
-        width: 100%;
-        padding: 1rem 0;
-        z-index: 2000;
-    }
+nav#primary-menu ul {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
 
-    header.shrink {
-        padding: 0.5rem 0;
-        background-color: #000;
-    }
+  row-gap: 4em;
+}
 
-    header.shrink #toggle-btn div {
-        background-color: #fff;
-    }
+nav#primary-menu {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: clamp(10rem, 40%, 20rem);
+  transform: translateX(200%);
+}
 
-    header .boxed {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        align-items: center;
-    }
+nav#primary-menu.active {
+  background-color: #000;
+  padding: 30% 10%;
+  transform: translateX(0);
+  transition: all 0.6 ease-in-out;
+}
 
-    nav#primary-menu ul {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        align-items: flex-start;
+nav#primary-menu.active a.navlinks {
+  color: #fff;
+}
 
-        row-gap: 4em;
-    }
-
-    nav#primary-menu {
-        position: absolute;
-        top: 0;
-        right: 0;
-        width: clamp(10rem, 40%, 20rem);
-        transform: translateX(200%);
-    }
-
-    nav#primary-menu.active {
-        background-color: #000;
-        padding: 30% 10%;
-        transform: translateX(0);
-        transition: all 0.6 ease-in-out;
-    }
-
-
-    nav#primary-menu.active a.navlinks {
-        color: #fff;
-    }
-
-
-
-    /* nav#primary-menu ul li,
+/* nav#primary-menu ul li,
     nav#primary-menu ul li a {
         width: fit-content;
     } */
 
+/*---------------------------------  CSS Animations ----------------------------*/
 
+@keyframes navLinksFade {
+  from {
+    opacity: 0;
+    transform: translateX(-50px);
+  }
 
+  to {
+    opacity: 1;
+    transform: translateX(0px);
+  }
+}
 
+@media only screen and (min-width: 1025px) {
+  header #toggle-btn {
+    display: none;
+  }
 
-       /*---------------------------------  CSS Animations ----------------------------*/
+  nav#primary-menu {
+    position: static;
+    transform: translate(0);
+  }
 
-
-
-    @keyframes navLinksFade {
-        from {
-            opacity: 0;
-            transform: translateX(-50px);
-        }
-
-        to {
-            opacity: 1;
-            transform: translateX(0px);
-        }
-    }
-
-
-
-    @media only screen and (min-width: 1025px) {
-                header #toggle-btn {
-            display: none;
-        }
-
-        nav#primary-menu {
-            position: static;
-            transform: translate(0);
-        }
-
-        nav#primary-menu ul {
-            flex-direction: row;
-        }
-
-    }
-
+  nav#primary-menu ul {
+    flex-direction: row;
+  }
+}
 </style>
